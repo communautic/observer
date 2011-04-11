f274704ed474075c22bf7424cca3faea35068234
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2011-04-11 12:22:57 +0000

    Optimized print header

--- trunk/data/templates/pdfheader.php
+++ trunk/data/templates/pdfheader.php
@@ -8,12 +8,12 @@ if ( isset($pdf) ) {
 	$img_w = 2 * 72; // 2 inches, in points
 	$img_h = 28; // 1 inch, in points -- change these as required
 
-	$pdf->page_text(72, 10, "communautic Ebenbichler KG", $font, 7, array(0.4,0.4,0.4));
-	$pdf->page_text(72, 20, "Mühlenweg 9, A - 6068 Mils", $font, 7, array(0.4,0.4,0.4));
-	$pdf->page_text(72, 40, "Fon +43 (676) 5700-506", $font, 7, array(0.4,0.4,0.4));
-	$pdf->page_text(72, 50, "Fax +43 (676) 5700-505", $font, 7, array(0.4,0.4,0.4));
-	$pdf->page_text(72, 70, "office@communautic.com", $font, 7, array(0.4,0.4,0.4));
-	$pdf->page_text(72, 80, "www.communautic.com", $font, 7, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 10, "communautic Ebenbichler KG", $font, 8, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 20, "M&uuml;hlenweg 9, A - 6068 Mils", $font, 7, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 30, "Fon +43 (676) 5700-506", $font, 7, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 40, "Fax +43 (676) 5700-505", $font, 7, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 50, "office@communautic.com", $font, 7, array(0.4,0.4,0.4));
+	$pdf->page_text(84, 60, "www.communautic.com", $font, 7, array(0.4,0.4,0.4));
 
 	$pdf->image(CO_PATH_BASE . "/data/logo_print.jpg", "jpg", $w-60-$img_w, 10, $img_w, $img_h);
     
