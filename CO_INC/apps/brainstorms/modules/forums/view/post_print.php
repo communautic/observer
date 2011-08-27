abdacf83522cffa95b63427fe59218359a2028a7
Gunharth Randolf <gunharth@users.noreply.github.com>
2011-08-27 20:07:14 +0000

    SWITCH: 6.1.0

---
+++ CO_INC/apps/brainstorms/modules/forums/view/post_print.php
@@ -0,0 +1,15 @@
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style=" border: 0px;">
+  <tr>
+      <td style="font-size: 8pt; width: 190px; background-color: #B2A85B; border-bottom: 1px solid #77713D">
+      	<?php echo $post->user;?>
+      	<table border="0" cellpadding="0" cellspacing="0">
+      		<tr>
+                <td style="width: 40px;"><img src="<?php echo CO_FILES;?>/img/avatar.jpg" width="30" height="45" /></td>
+                <td style="width: 50px;"><?php echo $post->datetime;?></td>
+                <td width="27"><?php echo $img;?></td>
+      		</tr>
+      	</table>
+      </td>
+      <td class="greybg" style="padding: 2pt 2pt 2pt 2pt; border-bottom: 1px solid #77713D"><?php echo nl2br($post->text);?></td>
+	</tr>
+</table>
