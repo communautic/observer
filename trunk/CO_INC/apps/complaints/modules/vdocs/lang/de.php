7e6aa1bb28d30a52a3907d9e753b4e4fdcfe19d9
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2012-04-03 15:45:50 +0000

    UPDATE: checkpoint datepicker language clear UPDATE: complaints vdocs manual

--- trunk/CO_INC/apps/complaints/modules/vdocs/lang/de.php
+++ trunk/CO_INC/apps/complaints/modules/vdocs/lang/de.php
@@ -6,7 +6,7 @@ $lang["COMPLAINT_VDOC_VDOCS"] = 'Textbeiträge';
 $lang["COMPLAINT_VDOC_NEW"] = 'Neuer Textbeitrag';
 $lang["COMPLAINT_VDOC_ACTION_NEW"] = 'neuen Texbeitrag anlegen';
 
-$lang["COMPLAINT_VDOC_HELP"] = 'manual_projekte_textbeitraege.pdf';
+$lang["COMPLAINT_VDOC_HELP"] = 'manual_reklamationen_textbeitraege.pdf';
 
 // check for custom lang file
 $custom_lang = CO_PATH_BASE . "/lang/complaints/vdocs/de.php";
