2a96c5f71771602526d0acd6b4d17664151b168a
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2012-07-26 11:11:12 +0000

    UPDATE: contact context: added email and phone links

--- trunk/CO_INC/apps/contacts/view/contact_context.php
+++ trunk/CO_INC/apps/contacts/view/contact_context.php
@@ -8,8 +8,8 @@
 	<p><span rel="<?php echo($context->id);?>" class="loadContactExternal co-link"><?php echo($context->lastname);?> <?php echo($context->firstname);?></span></p>
     <div class="text11">
     <div style="overflow: hidden; padding: 0; height: 22px;"><?php echo(nl2br($context->company));?></div>
-    <?php echo($context->phone1);?><br />
-    <div style="overflow: hidden; padding: 0;"><?php echo($context->email);?></div><br />
+    <div class="regular"><a href="callto:<?php echo($context->phone1);?>"><?php echo($context->phone1);?></a></div>
+    <div class="regular"><a href="mailto:<?php echo($context->email);?>"><?php echo($context->email);?></a></div><br />
     <?php echo($context->address_line1);?><br />
     <?php echo($context->address_postcode);?> <?php echo($context->address_town);?><br /></div>
     </div>
