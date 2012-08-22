6de7f8e10cb645646268dc5fb202c7493cae2974
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2012-08-22 08:14:26 +0000

    BUG: Display bug in documents context

--- trunk/CO_INC/apps/complaints/modules/documents/view/context.php
+++ trunk/CO_INC/apps/complaints/modules/documents/view/context.php
@@ -1,5 +1,6 @@
 <div class="context">
-<div class="contact-dialog-header"><a href="complaints_documents" class="removeItem" uid="<?php echo($document->id);?>" field="<?php echo($document->field);?>"><?php echo $lang["GLOBAL_DELETE"];?></a>v
+<div class="contact-dialog-header"><a href="complaints_documents" class="removeItem" uid="<?php echo($document->id);?>" field="<?php echo($document->field);?>"><?php echo $lang["GLOBAL_DELETE"];?></a></div>
+	<div class="dialog-text-3">
     <?php  foreach($doc as $value) { ?>
     <a mod="complaints_documents" class="downloadDocument" rel="<?php echo $value->id;?>" title="Download"><?php echo($value->filename)?></a>
     <?php } ?>
--- trunk/CO_INC/apps/employees/modules/documents/view/context.php
+++ trunk/CO_INC/apps/employees/modules/documents/view/context.php
@@ -1,5 +1,6 @@
 <div class="context">
-<div class="contact-dialog-header"><a href="employees_documents" class="removeItem" uid="<?php echo($document->id);?>" field="<?php echo($document->field);?>"><?php echo $lang["GLOBAL_DELETE"];?></a>v
+<div class="contact-dialog-header"><a href="employees_documents" class="removeItem" uid="<?php echo($document->id);?>" field="<?php echo($document->field);?>"><?php echo $lang["GLOBAL_DELETE"];?></a></div>
+	<div class="dialog-text-3">
     <?php  foreach($doc as $value) { ?>
     <a mod="employees_documents" class="downloadDocument" rel="<?php echo $value->id;?>" title="Download"><?php echo($value->filename)?></a>
     <?php } ?>
