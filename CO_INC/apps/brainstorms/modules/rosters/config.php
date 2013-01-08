2c992e2860c02d5a330b6c8cc973c4b720d82bc7
Gunharth Randolf <gunharth@users.noreply.github.com>
2013-01-08 21:14:16 +0000

    UPDATE Removed rosters from brainstorms

--- CO_INC/apps/brainstorms/modules/rosters/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$brainstorms_rosters_filter = 1;
-define("brainstorms_rosters_bin" , 1);
-
-define("CO_TBL_BRAINSTORMS_ROSTERS"			,	TBL_PREFIX . "brainstorms_rosters" 		);
-define("CO_TBL_BRAINSTORMS_ROSTERS_COLUMNS"		,	TBL_PREFIX . "brainstorms_rosters_columns" 	);
-define("CO_TBL_BRAINSTORMS_ROSTERS_NOTES"		,	TBL_PREFIX . "brainstorms_rosters_notes" 	);
-
-?>
