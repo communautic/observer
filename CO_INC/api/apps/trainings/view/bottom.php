883b0750be5b0754e784a9396f5f0286a2575576
Gunharth Randolf <gunharth@users.noreply.github.com>
2013-03-05 13:12:13 +0000

    UPDATE: trainings api

---
+++ CO_INC/api/apps/trainings/view/bottom.php
@@ -0,0 +1,4 @@
+<?php include(CO_PATH_TEMPLATES . 'trainings/api_footer.html');?>
+</div>
+</body>
+</html>
--- CO_INC/api/apps/trainings/view/invitation.php
+++ CO_INC/api/apps/trainings/view/invitation.php
@@ -1,46 +1,43 @@
-<!DOCTYPE html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php echo $lang["APPLICATION_NAME"];?></title>
-<link rel="icon" type="image/x-icon" href="<?php echo CO_FILES;?>/img/favicon.ico" sizes="64x64" />
-<link href="<?php echo CO_FILES;?>/css/reset.css" rel="stylesheet" type="text/css" media="screen,projection" />
-<link href="<?php echo CO_PATH_URL;?>/data/templates/css/orders.css" rel="stylesheet" type="text/css" media="screen,projection" />
-<script src="<?php echo $protocol;?>ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="<?php echo CO_FILES;?>/js/libraries/jquery.form.js"></script>
-<script type="text/javascript">
-
-$(document).ready(function() {
-	
-	$('#com-orderform').ajaxForm({
-        success: function(data) {
-			if (data == 1) {
-				$('#com-orderform').html("Einladung erfolgreich gespeichert. Sie können das Browser-Fenster jetzt schließen.");
-			} else {
-				$("#loginFailed").fadeIn();
-			}
-        }
-    });
-
-});
-</script>
-</head>
-<body>
-Einladung
-    
-<?php
-while($row = mysql_fetch_array($result)) {
-	$memberid = $row['id'];
-}
-
-
-
-?>
-<form id="com-orderform" name="com_form" method="post" action="/?path=api/apps/trainings">
-<input type="hidden" name="request" value="acceptinvitation" />
-<input type="hidden" name="key" value="<?php echo $key;?>" />
-<input type="hidden" name="id" value="<?php echo $memberid;?>" />
-<p>&nbsp;</p>
-<input type="submit" name="button" id="button" value="bestätigen" />
-</form>
-</body>
-</html>
+<table width="715" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td height="22" class="text11">Trainingsveranstaltung</td>
+                    <td><b>„<?php echo $training->title;?>“</b></td>
+                </tr>
+            </table>
+<br />
+            
+            
+<table width="715" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td width="236" height="27" style="background-color: #e5e5e5;">&nbsp;</td>
+                    <td  style="background-color: #e5e5e5;"><strong><?php echo $response;?></strong></td>
+    </tr>
+            </table>
+            <br />
+<table width="715" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td height="22" class="text11">Veranstaltungsdaten</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+            <table width="715" border="0" cellpadding="0" cellspacing="0" class="greyText">
+                <tr>
+                    <td height="22" class="text11">Trainingsanbieter</td>
+                    <td><?php echo $training->company;?></td>
+                </tr>
+                <tr>
+                    <td height="22" class="text11">TrainerIn</td>
+                    <td><?php echo $training->team;?><?php echo $training->team_ct;?></td>
+                </tr>
+                <tr>
+                    <td height="22" class="text11">Trainingsart</td>
+                    <td><?php echo $training->training;?></td>
+                </tr>
+            </table><br />
+<?php include('training_cat'.$training->training_id.'.php'); ?><br />
+<table width="715" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+        <td width="236" height="27" style="background-color: #e5e5e5;">&nbsp;</td>
+        <td  style="background-color: #e5e5e5;"><strong>Vielen Dank für Ihre Rückmeldung!</strong></td>
+    </tr>
+</table>
