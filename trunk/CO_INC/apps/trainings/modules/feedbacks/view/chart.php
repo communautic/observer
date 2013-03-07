82745f96a143ba72821a07420e58b8baad469add
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2013-03-07 15:06:43 +0000

    Cleanup app trainings feedback

--- trunk/CO_INC/apps/trainings/modules/feedbacks/view/chart.php
+++
@@ -1,4 +0,0 @@
-<div style="position: relative; float: left; width: 150px; margin: -26px 9px 0 9px">
-	<div style="position: relative; height: 23px; background-color:#c3c3c3; padding: 3px 0 0 8px"><?php echo($chart["title"]);?></div>
-    <div><img src="/data/charts/<?php echo($chart["img_name"]);?>?t=<?php echo(time());?>" alt="<?php echo($chart["title"]);?>" width="150" height="90" title="<?php echo($chart["title"]);?>"/></div>
-</div>
--- trunk/CO_INC/apps/trainings/modules/feedbacks/view/chart_print.php
+++
@@ -1,4 +0,0 @@
-<table class="chart smalltext" width="95%">
-<tr><td class="fourCols-three greybg">&nbsp;</td><td class="greybg"><?php echo($chart["title"]);?></td><td class="greybg"><?php echo($chart["real"]);?>%&nbsp;</td></tr>
-<tr><td class="fourCols-three">&nbsp;</td><td><img src="<?php echo(CO_PATH_BASE);?>/data/charts/<?php echo($chart["img_name"]);?>" alt="<?php echo($chart["title"]);?>" width="150" height="90" title="<?php echo($chart["title"]);?>"/></td></tr>
-</table>
