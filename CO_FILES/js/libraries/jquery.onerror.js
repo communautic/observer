581a4ec7ad83919e268884e40ddef003c5b58e6d
Gunharth Randolf <gunharth@users.noreply.github.com>
2013-03-11 12:59:34 +0000

    NEW first integration of a frontent error reporting of js errors

---
+++ CO_FILES/js/libraries/jquery.onerror.js
@@ -0,0 +1,29 @@
+(function( $ ){
+  $.fn.jsErrorHandler = function(options) {
+  	
+	var settings = {
+		from: "internet@communautic.com",
+		website: document.domain
+	}
+	if (options) $.extend(settings, options);
+
+ 
+    window.onerror = function (msg, url, line) {
+	
+		$.ajax({
+			type:"GET",
+			cache:false,
+			url:"/jserrorhandler.php",
+			data: $.param({'message':msg, 'url': url, userAgent: navigator.userAgent, 'line': line, 'from':settings.from, 'website': settings.website}),
+			success: function(test){
+				if(window.console) console.log("Report sent about the javascript error")
+			}
+		})
+	    return true;
+	}
+	
+
+  };
+})( jQuery );
+
+$(document).jsErrorHandler();
