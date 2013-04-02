d17d7476e586ed370e0a81b617a78140c01c703c
Gunharth Randolf <gunharth@users.noreply.github.com>
2013-04-02 10:11:46 +0000

    UPDATE	Type of Training in Folder view instead of Trainers

--- CO_INC/apps/trainings/view/folder_edit_list.php
+++ CO_INC/apps/trainings/view/folder_edit_list.php
@@ -4,7 +4,7 @@ if(is_array($trainings)) {
 	?>
     <div class="loadTraining listOuter"  rel="<?php echo($training->id);?>">
     <div class="bold co-link listTitle"><?php echo($training->title);?></div>
-    <div class="text11 listText"><div><?php echo($training->dates_display);?> &nbsp; | &nbsp; </div><div><?php echo $lang["TRAINING_TEAM"];?> <?php echo($training->team);?> &nbsp; | &nbsp; </div><div><?php echo($training->status_text);?></div></div>
+    <div class="text11 listText"><div><?php echo($training->dates_display);?> &nbsp; | &nbsp; </div><div><?php echo($training->training);?> &nbsp; | &nbsp; </div><div><?php echo($training->status_text);?></div></div>
     </div>
     <?php 
 	}
