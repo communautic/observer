e945e9bf209f39fa7a7bb3047e79cdea2cbe7010
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2013-08-16 12:57:57 +0000

    UPDATE patients removed obsolete comments folder

--- trunk/CO_INC/apps/patients/modules/comments/view/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(is_array($comments)) {
-foreach ($comments as $comment) {
-	echo('<li id="commentItem_'.$comment->id.'"><span rel="'.$comment->id.'" class="module-click"><span class="module-access-status'.$comment->accessstatus.'"></span><span class="text">' . $comment->item_date . ' - ' .$comment->title.'</span><span class="module-item-status'.$comment->itemstatus.'"></span><span class="icon-checked-out '.$comment->checked_out_status.'"></span></span></li>');
-}
-} else {
-	echo('<li></li>');
-}
-?>
