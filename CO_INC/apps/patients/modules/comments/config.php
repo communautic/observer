48706c0266d726b02150ecb3d47cc91b439af364
Gunharth Randolf <gunharth@users.noreply.github.com>
2013-08-16 12:57:57 +0000

    UPDATE patients removed obsolete comments folder

--- CO_INC/apps/patients/modules/comments/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$patients_comments_filter = 1;
-define("patients_comments_bin" , 1);
-define("CO_TBL_PATIENTS_COMMENTS"			,	TBL_PREFIX . "patients_comments" 		);
-define("CO_TBL_PATIENTS_COMMENTS_TASKS"		,	TBL_PREFIX . "patients_comments_tasks" 	);
-?>
