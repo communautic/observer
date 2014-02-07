1de4d8d97727a6752580f997615d8f0efb72704c
gunharth.randolf <gunharth.randolf@c9372852-adf6-4559-8435-1b00bf0be1f8>
2014-02-07 13:26:12 +0000

    UPDATE PO overviews and langs

--- trunk/CO_INC/apps/patients/modules/treatments/view/task.php
+++ trunk/CO_INC/apps/patients/modules/treatments/view/task.php
@@ -44,7 +44,7 @@
     </tr>
     <tr>
          <td width="12"></td>
-		<td class="tcell-left-inactive text11"><span><span>Kosten</span></span></td>
+		<td class="tcell-left-inactive text11"><span><span><?php echo $lang["PATIENT_TREATMENT_AMOUNT"];?></span></span></td>
 		<td class="tcell-right-inactive"><?php echo CO_DEFAULT_CURRENCY;?> <span id="costs_<?php echo $value->id;?>" class="currency"><?php echo number_format($value->costs,2,',','.');;?></span></td>
     </tr>
     </table>
