2f381214639ba8398f64da5609a87cb80b3041c9
Gunharth Randolf <gunharth@users.noreply.github.com>
2014-02-13 14:17:26 +0000

    UPDATE patients en translation

--- CO_INC/apps/desktop/lang/en.php
+++ CO_INC/apps/desktop/lang/en.php
@@ -7,6 +7,7 @@ $lang["WIDGET_TITLE_CHECKPOINTS"] 	= 	'Checkpoints';
 $lang["WIDGET_TEXT_CHECKPOINTS"] 	= 	'Checkpoint set for ';
 
 $lang["WIDGET_TITLE_PROJECTS"] 	= 	'Projects';
+$lang["WIDGET_TITLE_PATIENTS"] 	= 	'Patients';
 $lang["WIDGET_TITLE_PROCS"] 	= 	'Workplan';
 $lang["WIDGET_TITLE_FORUMS"] 	= 	'Forums';
 $lang["WIDGET_TITLE_SEARCH"] 	= 	'Search';
