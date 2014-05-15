a216e091649cf5c3d0510c0195f6eb927fb6d24c
Gunharth Randolf <gunharth@users.noreply.github.com>
2014-05-15 13:00:12 +0000

    UPDATE patients invoices print versions

--- CO_INC/apps/patients/modules/invoices/view/print_reminder.php
+++ CO_INC/apps/patients/modules/invoices/view/print_reminder.php
@@ -5,7 +5,7 @@
         <p class="smalltext" style="line-height: 15px;"><?php echo($invoice->m_email)?></p>
         <p class="smalltext" style="line-height: 10px;">&nbsp;</p>
         <p class="smalltext" style="line-height: 15px;">Patient: <?php echo($invoice->patient)?></p>
-        <p class="smalltext" style="line-height: 15px;"><?php echo $lang["PATIENT_INVOICE_NUMBER"];?>: <?php echo($invoice->invoice_number);?></p>
+        <p style="line-height: 15px;"><?php echo $lang["PATIENT_INVOICE_NUMBER"];?>: <?php echo($invoice->invoice_number);?></p>
         <p style="line-height: 25px;"><strong><?php echo strtoupper($lang["PATIENT_INVOICE_PAYMENT_REMINDER"]);?></strong></p>
 	</tr>
 </table>
@@ -14,17 +14,15 @@
         <td><p style="line-height: 25px;"><?php echo($invoice->ctitle)?> <?php echo($invoice->title2)?> <?php echo($invoice->lastname);?> <?php echo($invoice->firstname);?></p>
         <p style="line-height: 25px;"><?php echo($invoice->address_line1)?></p>
         <p style="line-height: 25px;"><?php echo($invoice->address_postcode)?> <?php echo($invoice->address_town)?></p></td>
-        <td valign="bottom" style="text-align: right;"><?php echo date('d.m.Y');?></td>
+        <td valign="bottom" style="text-align: right;"><?php echo $invoice->payment_reminder;?></td>
     </tr>
 </table>
-<p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p><?php echo $lang["PATIENT_INVOICE_TEXT_CITATION"];?> <?php echo($invoice->ctitle)?> <?php echo(ucfirst(strtolower($invoice->lastname)))?>!</p>
 <p>&nbsp;</p>
 <p style="line-height: 20px;"><?php echo $lang["PATIENT_INVOICE_TEXT_LINE1"];?></p>
-<p>&nbsp;</p>
 <table width="100%" class="standard" style="border:1px solid #ccc;">
 	<?php 
 	$i = 1;
@@ -32,11 +30,11 @@
 		$checked = '';
 		if($value->status == 1 && is_array($value->type)) { ?>
       <tr>
-        <td width="20%" style="border-bottom:1px solid #ccc; padding: 6px 0;"><span style="margin-left: 7px;"><?php echo $i;?>. <?php echo $lang["PATIENT_TREATMENT_GOALS_SINGUAL"];?></span></td>
-        <td style="border-bottom:1px solid #ccc; padding: 8px 0;"><span class="smalltext" style="line-height: 20px;"> (<?php echo $value->item_date;?>)</span></td>
-        <td style="border-bottom:1px solid #ccc; padding: 8px 0;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 20px;">' . $t['positionstext'] . ' ' . $t['shortname'] . '</span><br />'; } ?></td>
-        <td style="border-bottom:1px solid #ccc; padding: 8px 0;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 20px;">' . $t['minutes'] . 'min.</span><br />'; }?></td>
-        <td style="padding: 8px 0; text-align: right; border-left: 1px solid #ccc; border-bottom: 1px solid #ccc;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 20px;">' . CO_DEFAULT_CURRENCY . ' ' . $t['costs'] . ' &nbsp; &nbsp; </span><br />'; } ?> </td>
+        <td width="20%" style="border-bottom:1px solid #ccc; padding: 6px 0 0 0;"><span style="margin-left: 7px;"><?php echo $i;?>. <?php echo $lang["PATIENT_TREATMENT_GOALS_SINGUAL"];?></span></td>
+        <td style="border-bottom:1px solid #ccc; padding: 9px 0 4px 0;"><span class="smalltext" style="line-height: 20px;"> (<?php echo $value->item_date;?>)</span></td>
+        <td style="border-bottom:1px solid #ccc; padding: 9px 0 4px 0;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 15px;">' . $t['positionstext'] . ' ' . $t['shortname'] . '</span><br />'; } ?></td>
+        <td style="border-bottom:1px solid #ccc; padding: 9px 0 4px 0;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 15px;">' . $t['minutes'] . 'min.</span><br />'; }?></td>
+        <td style="padding: 9px 0 4px 0; text-align: right; border-left: 1px solid #ccc; border-bottom: 1px solid #ccc;" class="smalltext"><?php foreach($value->type as $t) { echo '<span style="line-height: 15px;">' . CO_DEFAULT_CURRENCY . ' ' . $t['costs'] . ' &nbsp; &nbsp; </span><br />'; } ?> </td>
     </tr>
    
 <?php }
@@ -73,6 +71,10 @@
 <p style="line-height: 20px;"><?php echo $lang["PATIENT_INVOICE_TEXT_LINE2"];?></p>
 <p>&nbsp;</p>
 <p style="line-height: 20px;"><?php echo $lang["PATIENT_INVOICE_TEXT_LINE3"];?></p>
-<p>&nbsp;<br /><br /></p>
+<p>&nbsp;</p>
 <p style="line-height: 20px;"><?php echo $lang["PATIENT_INVOICE_TEXT_LINE4"];?></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><?php echo($invoice->m_firstname)?> <?php echo($invoice->m_lastname)?></p>
 <div style="page-break-after:always;">&nbsp;</div>
