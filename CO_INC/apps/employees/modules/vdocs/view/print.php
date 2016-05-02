34bdff15e50041948cf764fafce687e003acc76f
Gunharth Randolf <gunharth@users.noreply.github.com>
2016-05-02 10:44:51 +0000

    Fixed tbl print for dompdf

--- CO_INC/apps/employees/modules/vdocs/view/print.php
+++ CO_INC/apps/employees/modules/vdocs/view/print.php
@@ -1,10 +1,4 @@
-<table width="100%" class="standard">
-	<tr>
-		<td>
-    <?php 
-		$content = str_replace('<!-- pagebreak -->','<div style="page-break-after:always;">&nbsp;</div>',$vdoc->content);
-		echo nl2br($content);
-		?>
-    </td>
-    </tr>
-</table>
+<?php 
+$content = str_replace('<!-- pagebreak -->','<div style="page-break-after:always;">&nbsp;</div>',$vdoc->content);
+echo nl2br($content);
+?>
